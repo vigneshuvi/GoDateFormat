2409813f50902cf2e1478fba74e8141cb527c04a
Vignesh <vigneshuvi@gmail.com>
2017-07-04 18:27:12 +0530

    Updated the example

--- Examples/Main.go
+++ Examples/Main.go
@@ -4,13 +4,14 @@ package main
 import (
     "fmt"
     "time"
-    "github.com/vigneshuvi/GoDateFormat"
+    "./GoDateFormat"
 )
 
 func main() {
     fmt.Println("Go Date Format(Today - 'yyyy-MM-dd HH:mm:ss Z'): ", GetToday(GoDateFormat.ConvertFormat("yyyy-MM-dd HH:mm:ss Z")))
     fmt.Println("Go Date Format(Today - 'yyyy-MMM-dd'): ", GetToday(GoDateFormat.ConvertFormat("yyyy-MMM-dd")))
-    fmt.Println("Go Time Format(NOW - 'HH:mm:ss'): ", GetToday(GoDateFormat.ConvertFormat("HH:MM:SS")))
+    fmt.Println("Go Time Format(NOW - 'HH:MM:SS'): ", GetToday(GoDateFormat.ConvertFormat("HH:MM:SS")))
+    fmt.Println("Go Time Format(NOW - 'HH:MM:SS tt'): ", GetToday(GoDateFormat.ConvertFormat("HH:MM:SS tt")))
 }
 
 func GetToday(format string) (todayString string){
